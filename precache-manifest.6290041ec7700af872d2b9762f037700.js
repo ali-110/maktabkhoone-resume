e6044454091302b63eb7220ce270ccb2a63cc409
ali-110 <ali31sebti@gmail.com>
2020-04-01 19:32:59 +0430

    Updates

---
+++ precache-manifest.6290041ec7700af872d2b9762f037700.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83ce3568164746694b7ec8078bd186dd",
+    "url": "/maktabkhoone-resume/index.html"
+  },
+  {
+    "revision": "7b222db4c8fa52ec00c2",
+    "url": "/maktabkhoone-resume/static/css/main.931f44c5.chunk.css"
+  },
+  {
+    "revision": "dc106cb9a46c298f0a4d",
+    "url": "/maktabkhoone-resume/static/js/2.f12c3fd8.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/maktabkhoone-resume/static/js/2.f12c3fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b222db4c8fa52ec00c2",
+    "url": "/maktabkhoone-resume/static/js/main.9e5703d2.chunk.js"
+  },
+  {
+    "revision": "39d37b215c5e44ff9cdb",
+    "url": "/maktabkhoone-resume/static/js/runtime-main.adefd05d.js"
+  }
+]);
